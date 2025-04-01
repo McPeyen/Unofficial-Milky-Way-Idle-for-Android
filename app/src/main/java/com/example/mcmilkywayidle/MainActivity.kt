eb136226f7b5f4bbb09e995b49b72b53178d87cf
McPeyen <jdfhawkins@outlook.com>
2025-04-01 21:38:48 +0100

    Removed the toolbar and embedded Script Manager into Game settings

--- app/src/main/java/com/example/mcmilkywayidle/MainActivity.kt
+++ app/src/main/java/com/example/mcmilkywayidle/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.webkit.JavascriptInterface
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -23,13 +24,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-
         // Initialize ScriptManager
         scriptManager = ScriptManager(this)
-
 
         // Initialize WebView
         webView = findViewById(R.id.webview)
@@ -41,7 +37,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
         // Configure WebView settings
         val webSettings = webView!!.settings
         webSettings.javaScriptEnabled = true
@@ -49,10 +44,8 @@ class MainActivity : AppCompatActivity() {
         webSettings.databaseEnabled = true
         webSettings.cacheMode = WebSettings.LOAD_DEFAULT
 
-
         // Load Milky Way Idle website
         webView!!.loadUrl("https://www.milkywayidle.com/")
-
 
         // Check for script updates
         updateScripts()
@@ -94,6 +87,75 @@ class MainActivity : AppCompatActivity() {
         injectScripts()
     }
 
+    private fun injectSettings(webView: WebView) {
+        val jsCode = """
+        (function mcMwiSettings () {
+            const targetNode = document.querySelector("div.SettingsPanel_gameTab__n2hAG");
+            if (targetNode) {
+                // Create container div to hold text and button
+                let container = document.createElement("div");
+                container.style.display = "flex";
+                container.style.alignItems = "center";
+                container.style.margin = "10px 0";
+                
+                // Create text label
+                let label = document.createElement("span");
+                label.innerHTML = "Open Script Manager: ";
+                label.style.marginRight = "10px";
+            
+                // Create button with icon
+                let button = document.createElement("button");
+                button.style.backgroundColor = "#4357af";
+                button.style.color = "white";
+                button.style.border = "none";
+                button.style.borderRadius = "4px";
+                button.style.padding = "5px 10px";
+                button.style.cursor = "pointer";
+                button.style.display = "flex";
+                button.style.alignItems = "center";
+                button.style.justifyContent = "center";
+            
+                // Add icon (using a simple SVG code icon)
+                button.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right: 5px;">
+                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                        <polyline points="14 2 14 8 20 8"></polyline>
+                        <line x1="16" y1="13" x2="8" y2="13"></line>
+                        <line x1="16" y1="17" x2="8" y2="17"></line>
+                        <polyline points="10 9 9 9 8 9"></polyline>
+                    </svg>
+                    Script Manager
+                `;
+            
+                // Add click event listener to call openScriptManager()
+                button.addEventListener("click", function() {
+                    window.Android.openScriptManager();
+                });
+            
+                // Assemble and add to the page
+                container.appendChild(label);
+                container.appendChild(button);
+                targetNode.insertAdjacentElement("beforeend", container);
+            
+            } else {
+                setTimeout(mcMwiSettings, 500);
+            }
+        })();
+    """.trimIndent()
+
+        webView.addJavascriptInterface(WebAppInterface(), "Android")
+        webView.evaluateJavascript(jsCode, null)
+    }
+
+    private inner class WebAppInterface {
+        @JavascriptInterface
+        fun openScriptManager() {
+            // Call your existing openScriptManager method
+            this@MainActivity.openScriptManager()
+        }
+    }
+
+
     private fun openScriptManager() {
         val intent = Intent(this, ScriptManagerActivity::class.java)
         ContextCompat.startActivity(this, intent, null) // Null for options
@@ -115,6 +177,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun injectScripts() {
+        injectSettings(webView!!)
         scriptManager!!.injectEnabledScripts(webView!!)
     }
 }
