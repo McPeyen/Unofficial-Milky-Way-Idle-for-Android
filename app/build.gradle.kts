af5c2bd306dd16cf385d1c1df0ee32f28a3e1677
McPeyen <jdfhawkins@outlook.com>
2025-04-19 09:43:17 +0100

    Updated version to 0.25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "0.21"
+        versionName = "0.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
